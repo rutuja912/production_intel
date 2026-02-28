7fe7f4bdfc2da4f9f63bd5aaea3530b8b45102c2
rutujads <rshitole_t@tataunistore.com>
2026-02-28 19:11:57 +0530

    db.py changes

--- Production Intel/backend/database.py
+++ Production Intel/backend/database.py
@@ -5,8 +5,7 @@ import os
 # DATABASE_URL = "sqlite:///./factory.db"
 DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    DATABASE_URL
 )
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
